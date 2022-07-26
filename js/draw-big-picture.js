b62d5eac5c60dcd7a396c2d9c518c90fce9d1702
Stuloviv <stuloviv@gmail.com>
2022-07-26 09:55:03 +0400

    Добавляет скрытие комментариев и их счётчики

--- js/draw-big-picture.js
+++ js/draw-big-picture.js
@@ -42,36 +42,30 @@ function addComments (commentsObject) {
 function drawPicture (pictureSource, pictureLikesCount, pictureComments, pictureDescription) {
   image.querySelector('img').src = pictureSource;
   picture.querySelector('.likes-count').textContent = pictureLikesCount;
-  // const commentsCount = picture.querySelector('.comments-count').textContent = pictureComments.length;
   picture.querySelector('.social__caption').textContent = pictureDescription;
-  // const commentCount = document.querySelector('.social__comment-count');
+  const commentCount = document.querySelector('.social__comment-count');
 
-  if (pictureComments.length <= 5) {
-    commentsLoader.classList.add('hidden');
-    addComments(pictureComments);
-    // commentCount.textContent = `${commentsCount} из ${commentsCount} комментариев`;
-  } else {
-    commentsLoader.classList.remove('hidden');
-    addComments(pictureComments.slice(0, 5));
-    let loadCount = 1;
-    commentsLoader.addEventListener('click', () => {
-      loadCount++;
-      addComments(pictureComments.slice(0, 5 * loadCount));
+  let loadCount = 0;
 
-      if (pictureComments.length === pictureComments.slice(0, 5 * loadCount).length) {
-        commentsLoader.classList.add('hidden');
-      }
-      // commentCount.textContent = `${pictureComments.slice(0, 5 * loadCount).length} из ${commentsCount} комментариев`;
-    });
+  function setComments () {
+    loadCount++;
+    const comments = pictureComments.slice(0, 5 * loadCount);
+    addComments(comments);
+    commentCount.textContent = `${comments.length} из ${pictureComments.length} комментариев`;
+    if (comments.length === pictureComments.length) {
+      commentsLoader.classList.add('hidden');
+      commentsLoader.removeEventListener('click', setComments);
+    } else {
+      commentsLoader.classList.remove('hidden');
+    }
   }
+  commentsLoader.addEventListener('click', setComments);
+  setComments();
 }
-// Открыть со всеми вытекающими
-// const socialCommentCount = picture.querySelector('.social__comment-count');
 
+// Открыть со всеми вытекающими
 function openPicture () {
   picture.classList.remove('hidden');
-  // socialCommentCount.classList.add('hidden');
-  // commentsLoader.classList.add('hidden');
   document.body.classList.add('modal-open');
 
   // Обработчик добавляется только при открытии окна
